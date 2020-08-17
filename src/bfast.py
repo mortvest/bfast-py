535dfc86d7f1226468c72de835d9dae73c7d23d1
mortvest <dmitry.serykh@gmail.com>
2020-08-17 17:19:11 +0200

    logging added, bfast not working

--- src/bfast.py
+++ src/bfast.py
@@ -71,7 +71,7 @@ class BFAST():
         Wt_bp = 0
         CheckTimeTt = 1
         CheckTimeSt = 1
-        i = 0
+        i = 1
         output = None
         nan_map = utils.nan_map(Yt)
 
@@ -80,23 +80,28 @@ class BFAST():
             CheckTimeTt = Vt_bp
             CheckTimeSt = Wt_bp
 
+            logger.info("BFAST iteration #{}".format(i))
             ### Change in trend component
             with np.errstate(invalid="ignore"):
                 Vt = Yt - St  # Deseasonalized Time series
-            logger.info("Vt:\n{}".format(Vt))
+            logger.debug("Vt:\n{}".format(Vt))
             p_Vt = EFP(sm.add_constant(ti), Vt, h).sctest()
             if p_Vt[1] <= level:
+                logger.info("Breakpoints in trend detected")
                 ti1, Vt1 = utils.omit_nans(ti, Vt)
+                logger.info("Finding breakpoints in trend")
                 bp_Vt = Breakpoints(sm.add_constant(ti1), Vt1, h=h, breaks=breaks, use_mp=use_mp)
-                if bp_VT.breakpoints is not None:
+                if bp_Vt.breakpoints is not None:
                     bp_Vt.breakpoints = np.array([nan_map[i] for i in bp_Vt.breakpoints])
                     nobp_Vt = False
                 else:
                     nobp_Vt = True
             else:
+                logger.info("No breakpoints in trend detected")
                 nobp_Vt = True
                 bp_Vt = None
 
+            logger.info("Fitting linear model for trend")
             if nobp_Vt:
                 ## No Change detected
                 fm0 = sm.OLS(Vt, ti, missing='drop').fit()
@@ -126,7 +131,10 @@ class BFAST():
                     Wt = Yt - Tt
                 p_Wt = EFP(sm.add_constant(smod), Wt, h).sctest()  # preliminary test
                 if p_Wt[1] <= level:
+                    logger.info("Breakpoints in season detected")
                     smod1, Wt1 = utils.omit_nans(smod, Wt1)
+
+                    logger.info("Finding breakpoints in season")
                     bp_Wt = Breakpoints(sm.add_constants(mod1), Wt1, h=h, breaks=breaks)
                     if bp_Wt.breakpoints is not None:
                         bp_Wt.breakpoints = np.array([nan_map[i] for i in bp_Wt.breakpoints])
@@ -134,9 +142,11 @@ class BFAST():
                     else:
                         nobp_Wt = True
                 else:
+                    logger.info("No breakpoints in season detected")
                     nobp_Wt = True
                     bp_Wt = None
 
+                logger.info("Fitting linear model for season")
                 if nobp_Wt:
                     ## No seasonal change detected
                     sm0 = sm.OLS(Wt, smod, missing='drop').fit()
@@ -160,43 +170,46 @@ class BFAST():
             with np.errstate(invalid="ignore"):
                 Nt = Yt - Tt - St
 
+            i += 1
             output = BFASTResult(Tt, St, Nt, Vt_bp, Wt_bp)
 
-        # if not nobp_Vt: # probably only works well for dummy model!
-        #     Vt_nrbp = Vt_bp.shape[0] if Vt_bp is not None else 0
-        #     co = coef(fm1) # final fitted trend model
-        #     Mag = matrix(NA, Vt.nrbp, 3)
-        #     for (r in 1:Vt.nrbp):
-        #         if r == 1:
-        #             y1 = co[1] + co[r + Vt.nrbp + 1] * ti[Vt.bp[r]]
-        #         else:
-        #             y1 = co[1] + co[r] + co[r + Vt.nrbp + 1] * ti[Vt.bp[r]]
+        if not nobp_Vt: # probably only works well for dummy model!
+            logger.info("Calculating breakpoint magnitude")
+            Vt_nrbp = Vt_bp.shape[0] if Vt_bp is not None else 0
+            co = fm1.params  # final fitted trend model
+            Mag = np.tile(np.nan, Vt_nrbp, 3)
+            # for (r in 1:Vt.nrbp):
+            for r in range(Vt_nrbp):
+                if r == 1:
+                    y1 = co[0] + co[r + Vt_nrbp + 1] * ti[Vt_bp[r]]
+                else:
+                    y1 = co[0] + co[r] + co[r + Vt_nrbp + 1] * ti[Vt_bp[r]]
 
-        #         y2 = (co[1] + co[r + 1]) + co[r + Vt.nrbp + 2] * ti[Vt.bp[r] + 1]
+                y2 = (co[0] + co[r + 1]) + co[r + Vt_nrbp + 2] * ti[Vt_bp[r] + 1]
 
-        #         Mag[r, 0] = y1
-        #         Mag[r, 1] = y2
-        #         Mag[r, 2] = y2 - y1
+                Mag[r, 0] = y1
+                Mag[r, 1] = y2
+                Mag[r, 2] = y2 - y1
 
-        #     index = which.max(abs(Mag[, 3]))
-        #     m_x = rep(Vt.bp[index], 2)
-        #     m_y = c(Mag[index, 1], Mag[index, 2]) #Magnitude position
-        #     Magnitude = Mag[index, 3] # Magnitude of biggest change
-        #     Time = Vt_bp[index]
-        # else:
-        #     m_x = None
-        #     m_y = None
-        #     Magnitude = 0 # if we do not detect a break then the magnitude is zero
-        #     Time = None # if we do not detect a break then we have no timing of the break
-        #     Mag = 0
+            index = np.argmin(np.abs(Mag[:, 2]) - 1)
+            m_x = np.repeat(Vt_bp[index], 2)
+            m_y = Mag[index, :2]  # Magnitude position
+            Magnitude = Mag[index, 2]  # Magnitude of biggest change
+            Time = Vt_bp[index]
+        else:
+            m_x = None
+            m_y = None
+            Magnitude = 0  # if we do not detect a break then the magnitude is zero
+            Time = None  # if we do not detect a break then we have no timing of the break
+            Mag = 0
 
         self.Yt = Yt
         self.output = output
         self.nobp = (nobp_Vt, nobp_Wt)
-        # self.magnitude = Magnitude
-        # self.mags = mags
-        # self.time = Time
-        # self.jump = [ti[m_x], m_y]
+        self.magnitude = Magnitude
+        self.mags = Mag
+        self.time = Time
+        self.jump = (ti[m_x], m_y)
 
 
 if __name__ == "__main__":
